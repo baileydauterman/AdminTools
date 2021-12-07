4d51ddf6f869fc7c34ff0e3a3c6ace266bfafac7
baileydauterman <50151523+baileydauterman@users.noreply.github.com>
2021-12-08 00:52:11 +0300

    add remote users and change output for uptime

--- Get-ComputerUptime.ps1
+++ Get-ComputerUptime.ps1
@@ -1,7 +1,6 @@
 function Get-ComputerUptime($ComputerName) {
     if($null -eq $ComputerName) {
-        $uptime = (Get-Date) - (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
-        return $uptime
+        return (Get-Date) - (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
     } elseif(Test-Connection $ComputerName -Count 1 -Quiet) {
         return (Get-Date) - (Get-CimInstance Win32_OperatingSystem -ComputerName $ComputerName).LastBootUpTime
     } 
