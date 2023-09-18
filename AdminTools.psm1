86b837e260980f860edfff912f012a820c8898a0
baileydauterman <50151523+baileydauterman@users.noreply.github.com>
2023-09-18 18:47:54 -0400

    Update AdminTools.psm1

--- AdminTools.psm1
+++ AdminTools.psm1
@@ -1,2 +1,4 @@
+Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Namespace WinNative -Name User32
+
 # Load functions as .\tools into memory
 Get-ChildItem -Path "$PSScriptRoot\tools\" -Filter *.ps1 -Recurse | ForEach-Object { . $_.FullName}
