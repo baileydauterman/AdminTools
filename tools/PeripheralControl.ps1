b68f4c1903194fc1c8dd98f6ab8ff4585a1deb68
baileydauterman <50151523+baileydauterman@users.noreply.github.com>
2023-09-18 18:29:04 -0400

    Update PeripheralControl.ps1

--- tools/PeripheralControl.ps1
+++ tools/PeripheralControl.ps1
@@ -4,7 +4,9 @@ enum ClickType {
 }
 
 class PeripheralControl {
-    <# Define the class. Try constructors, properties, or methods. #>
+    <#
+    Constructor for the class
+    #>
     PeripheralControl() {
         Add-Type -AssemblyName System.Windows.Forms
         Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Name U32 -Namespace Win;
@@ -12,10 +14,16 @@ class PeripheralControl {
 
     [System.__ComObject]$WShell = [System.__ComObject](New-Object -ComObject WScript.Shell)
 
+    <#
+    Send a key or a string of multiple keys to be sent to the os
+    #>
     [void]SendKeyPress([string]$Keys) {
         $this.WShell.SendKeys($Keys)
     }
 
+    <#
+    Send a mouse click event. Left or Right based on ClickType enum
+    #>
     [void]SendMouseClick([ClickType]$ClickType) {
         switch ($ClickType) {
             [ClickType]::Left  { [Win.U32]::mouse_event(6, 0, 0, 0, 0); }
@@ -23,10 +31,16 @@ class PeripheralControl {
         }
     }
 
+    <#
+    Move mouse to an X, Y coordinate
+    #>
     [void]MoveMousePosition([int]$X, [int]$Y) {
         [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($X, $Y)
     }
 
+    <#
+    Get current X,Y coordinate of the mouse
+    #>
     [PSCustomObject]GetMousePosition(){
         return [System.Windows.Forms.Cursor]::Position | Select-Object X, Y
     }
